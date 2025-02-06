cdc5d554ec03e766307ebe95c3e7a152aaf00a72
Benaya A.S <84769509+Benezerds@users.noreply.github.com>
2025-02-06 22:08:22 +0700

    Added relations between tables in schema

--- src/drizzle/schema.ts
+++ src/drizzle/schema.ts
@@ -1,11 +1,25 @@
 import { DAYS_OF_WEEK_IN_ORDER } from "@/data/constants";
-import {boolean, index, integer, pgEnum, pgTable, text, timestamp, uuid} from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+import {
+  boolean,
+  index,
+  integer,
+  pgEnum,
+  pgTable,
+  text,
+  timestamp,
+  uuid,
+} from "drizzle-orm/pg-core";
 
-const createdAt = timestamp("createdAt").notNull().defaultNow()
-const updatedAt = timestamp("updatedAt").notNull().defaultNow().$onUpdate(() => new Date())
+const createdAt = timestamp("createdAt").notNull().defaultNow();
+const updatedAt = timestamp("updatedAt")
+  .notNull()
+  .defaultNow()
+  .$onUpdate(() => new Date());
 
-
-export const EventTable = pgTable("events", {
+export const EventTable = pgTable(
+  "events",
+  {
     id: uuid("id").primaryKey().defaultRandom(),
     name: text("name").notNull(),
     desription: text("description"),
@@ -14,24 +28,49 @@ export const EventTable = pgTable("events", {
     isActive: boolean("isActive").notNull().default(true),
     createdAt,
     updatedAt,
-}, table => ({
-    clerkUserIdIndex: index("clerkUserIdIndex").on(table.clerkUserId)
-}))
+  },
+  (table) => ({
+    clerkUserIdIndex: index("clerkUserIdIndex").on(table.clerkUserId),
+  })
+);
 
-export const shcescheduleDayOfWeekEnumdul = pgEnum("day", DAYS_OF_WEEK_IN_ORDER)
+export const shceduleDayOfWeekEnumdul = pgEnum("day", DAYS_OF_WEEK_IN_ORDER);
 
 export const ScheduleTable = pgTable("schedules", {
-    id: uuid("id").primaryKey().defaultRandom(),
-    timezone: text("timezone").notNull(),
-    clerkUserId: text("clerkUserId").notNull().unique(),
-    createdAt,
-    updatedAt,
-})
+  id: uuid("id").primaryKey().defaultRandom(),
+  timezone: text("timezone").notNull(),
+  clerkUserId: text("clerkUserId").notNull().unique(),
+  createdAt,
+  updatedAt,
+});
 
-export const ScheduleAvailabilityTable() = pgTable("scheduleAvailibities", {
+export const scheduleRelations = relations(ScheduleTable, ({ many }) => ({
+  availabilities: many(ScheduleAvailabilityTable),
+}));
+
+export const ScheduleAvailabilityTable = pgTable(
+  "scheduleAvailibities",
+  {
     id: uuid("id").primaryKey().defaultRandom(),
-    scheduleId: uuid("scheduleId").notNull().references(() => ScheduleTable.id, { onDelete = "cascade"}),
+    scheduleId: uuid("scheduleId")
+      .notNull()
+      .references(() => ScheduleTable.id, { onDelete: "cascade" }),
     startTime: text("startTime").notNull(),
     endTime: text("endTime").notNull(),
-    dayOfWeek: scheduleDayOfWeekEnum("dayOfWeek").notNull(),
-})
+    dayOfWeek: shceduleDayOfWeekEnumdul("dayOfWeek").notNull(),
+  },
+  (table) => ({
+    scheduleIdIndex: index("scheduleIdIndex").on(table.scheduleId),
+  })
+);
+
+export const ScheduleAvailabilityRelations = relations(
+  ScheduleAvailabilityTable,
+  ({}) => ({
+    //@ts-ignore
+    schedule: one(ScheduleTable, {
+      fields: [ScheduleAvailabilityTable.scheduleId],
+      references: [ScheduleTable.id],
+    }),
+  })
+);
