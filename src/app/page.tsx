fb0fe8cdfb4f9c16c6d2439faa8a1fd5de1456bc
Benaya A.S <84769509+Benezerds@users.noreply.github.com>
2025-02-06 15:12:14 +0700

    Installed and Utilized Clerk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import { Button } from "@/components/ui/button";
+import { SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
 
 export default function HomePage() {
   return (
     <div className="text-center container my-4 mx-auto">
       <h1 className="text-3xl mb-4">Home Page</h1>
-      <div className="flex gap-2 justify-center">
-        <Button>Sign In</Button>
-        <Button>Sign Up</Button>
+      <div className="flex gap-2 justify-center">  
+        <Button asChild><SignInButton mode="modal"/></Button>
+        <Button asChild><SignUpButton mode="modal"/></Button>
+        <UserButton />
       </div>
     </div>
   );
