afb7ec7fb05068f003ee39f078ccaee7440f6ce7
Benaya A.S <84769509+Benezerds@users.noreply.github.com>
2025-02-10 23:39:53 +0700

    Added Edit Event Page

---
+++ src/app/(private)/events/[eventId]/edit/page.tsx
@@ -0,0 +1,32 @@
+import EventForm from "@/components/forms/EventForm";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { db } from "@/drizzle/db";
+import { auth } from "@clerk/nextjs/server";
+
+async function EditEventPage({
+  params: { eventId },
+}: {
+  params: { eventId: string };
+}) {
+  const { userId, redirectToSignIn } = await auth();
+
+  if (userId == null) return redirectToSignIn();
+
+  const event = await db.query.EventTable.findFirst({
+    where: ({ id, clerkUserId }, { and, eq }) =>
+      and(eq(clerkUserId, userId), eq(id, eventId)),
+  });
+
+  return (
+    <Card className="max-w-md mx-auto">
+      <CardHeader>
+        <CardTitle>Edit Event</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <EventForm />
+      </CardContent>
+    </Card>
+  );
+}
+
+export default EditEventPage;
